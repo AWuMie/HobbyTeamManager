acc12ac4f2b645657226c2fe90dee096a1c9d758
Achim Mienhardt <59267410+AWuMie@users.noreply.github.com>
2022-04-06 15:57:20 +0200

    added some helper classes to generate and check passwords and mailaddresses

---
+++ Utilities/Checker.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace MySqlTestRazor.Utilities;
+
+public class Checker
+{
+    public static bool IsMailAddressCorrect(string mail)
+    {
+        try
+        {
+            var email = new MailAddress(mail);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("FEHLER: {0} / Art: {1}", e.Message, e.GetType());
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Does the provided password fit to the rules:
+    /// [1] minimum 8 characters
+    /// [2] minimum 1 capital letter, 1 lower case letter, 1 number
+    /// [3] minimum 1 special character from ",.-+;:_!§$"
+    /// </summary>
+    /// <param name="password">string</param>
+    /// <returns>true or false if fits or not</returns>
+    public static bool DoesPasswordFitRules(string password)
+    {
+        if (password.Length < 8)
+            return false;
+
+        var hasNumbers = new Regex(@"[0-9]+");
+        var hasCapitalLetters = new Regex(@"[A-Z]+");
+        var hasLowerCaseLetters = new Regex(@"[a-z]+");
+        var hasSpecialCharacters = new Regex(@"[,.+-;:_!§$]+");
+
+        return hasNumbers.IsMatch(password)
+            && hasCapitalLetters.IsMatch(password)
+            && hasLowerCaseLetters.IsMatch(password)
+            && hasSpecialCharacters.IsMatch(password);
+    }
+}
---
+++ Utilities/PasswordCryptography.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MySqlTestRazor.Utilities;
+
+public class PasswordCryptography
+{
+    public struct HashSalt
+    {
+        public byte[] hash;
+        public int salt;
+    }
+
+    public static HashSalt GenerateHashSaltFromPassword(string password)
+    {
+        var hs = new HashSalt();
+        hs.salt = GenerateSalt();
+        hs.hash = CalculateHash(password, hs.salt);
+        return hs;
+    }
+
+    private static int GenerateSalt()
+    {
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            var saltBytes = new byte[4];
+
+            rng.GetNonZeroBytes(saltBytes);
+
+            int salt =
+                (((int)saltBytes[0]) << 24) +
+                (((int)saltBytes[1]) << 16) +
+                (((int)saltBytes[2]) << 8) +
+                ((int)saltBytes[3]);
+
+            return salt;
+        }
+    }
+
+    private static byte[] CalculateHash(string password, int salt)
+    {
+        var saltBytes = new byte[4];
+        saltBytes[0] = (byte)(salt >> 24);
+        saltBytes[1] = (byte)(salt >> 16);
+        saltBytes[2] = (byte)(salt >> 8);
+        saltBytes[3] = (byte)(salt);
+
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+
+        var passwordAndSaltBytes = new byte[saltBytes.Length + passwordBytes.Length];
+        Buffer.BlockCopy(
+            passwordBytes, 0, passwordAndSaltBytes, 0, passwordBytes.Length);
+        Buffer.BlockCopy(
+            saltBytes, 0, passwordAndSaltBytes, passwordBytes.Length, saltBytes.Length);
+
+        var sha = SHA512.Create();
+        return sha.ComputeHash(passwordAndSaltBytes);
+    }
+
+    public static bool IsPasswordCorrect(string password, HashSalt hs)
+    {
+        var hash = CalculateHash(password, hs.salt);
+        return hash == hs.hash;
+    }
+}
