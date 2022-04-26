20caf0c889e13c3d057531228198a2b3df6f4b74
Achim Mienhardt <59267410+AWuMie@users.noreply.github.com>
2022-04-26 18:00:31 +0200

    refactoring for unit testing

--- Pages/Sites/Create.cshtml.cs
+++ Pages/Sites/Create.cshtml.cs
@@ -33,24 +33,11 @@ public class CreateModel : SiteBaseModel
         }
 
         var stream = await GetCheckResizeImageAsync<Site>();
-        Site.Logo = stream.ToArray();
-        
-        if (Site.TeamColor1 == Site.TeamColor2)
-        {
-            ConfirmationModeOptions = Utilities.Miscellaneous.PopulateDropDownList(Site.confirmationMode, "Key", "Value",
-                Site.ConfirmationModeId);
-            MenuPositionOptions = Utilities.Miscellaneous.PopulateDropDownList(Site.menuPosition, "Key", "Value",
-                Site.MenuPositionId);
-            return Page();
-        }
+        Site.Logo = stream?.ToArray();
 
         Context.Sites.Add(Site);
         await Context.SaveChangesAsync();
 
-        string referer = Request.Headers["Referer"].ToString();
-        if (referer != null && Url.IsLocalUrl(referer))
-            return RedirectToPage(referer);
-        
         return RedirectToPage("./Index");
     }
 }
